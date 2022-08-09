84c963770ccf7fe0a4154987c15f1e80ac1474d8
Nuralam30 <nuralam.hstu.930@gmail.com>
2022-08-09 21:10:23 +0600

    show phone number button added

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ function App() {
       .then( res => res.json())
       .then( data => setUsers(data.results))
   }, []);
-  console.log(users)
 
   return (
     <div className="App">
--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './User.css';
+import { useState } from 'react';
 
 const User = (props) => {
 
-    const {name, email, phone, picture} = props.user;
+    const {name, email, phone, picture, location} = props.user;
+    const fullName = name.first + ' ' + name.last;
+    const [phoneNum, setPhoneNum] = useState();
+
+    const showPhone = () => setPhoneNum(phone)
 
     return (
         <div className='single-user'>
@@ -11,9 +16,11 @@ const User = (props) => {
                 <img src={picture.large} alt="user thumbnail" />
             </div>
             <div className="user-info">
-                <h3>Name : {name.first + ' ' + name.last}</h3>
+                <h3>Name : {fullName}</h3>
                 <p>email : {email}</p>
-                <p>Phone : {phone}</p>
+                <p>Location : {location.city + ', ' + location.country}</p>
+                <p>Phone : {phoneNum}</p>
+                <button onClick={showPhone} style={{marginRight: '10px'}}>Show Phone</button>
                 <button>Add me</button>
             </div>
         </div>
